9caa5c9dbb9eedb4cfba0bf6c25bf3b37040eb36
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-01 15:19:06 +0330

    Linking the layer double player.xml to its activity double_Player.java

--- app/src/main/java/com/example/tictactoe/double_Player.java
+++ app/src/main/java/com/example/tictactoe/double_Player.java
@@ -1,13 +1,23 @@
 package com.example.tictactoe;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class double_Player extends AppCompatActivity {
-
+    TextView txt_player1,txt_player2,txt_Score_1,txt_Score_2;
+    ImageView img_0,img_1,img_2,img_3,img_4,img_5,img_6,img_7,img_8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_double_player);
+        txt_player1=findViewById(R.id.txt_player1);   txt_player2=findViewById(R.id.txt_player2);
+        txt_Score_1=findViewById(R.id.txt_Score_1);   txt_Score_2=findViewById(R.id.txt_Score_2);
+        img_0=findViewById(R.id.img_0);  img_1=findViewById(R.id.img_1);  img_2=findViewById(R.id.img_2);
+        img_3=findViewById(R.id.img_3);  img_4=findViewById(R.id.img_4);  img_5=findViewById(R.id.img_5);
+        img_6=findViewById(R.id.img_6);  img_7=findViewById(R.id.img_7);  img_8=findViewById(R.id.img_8);
+
     }
 }
